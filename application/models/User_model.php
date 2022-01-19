2092c4465a6e7277e1eb986b6c936a3eef7ec9bf
SyfaCollins <coniplexy@gmail.com>
2022-01-20 01:33:46 +0300

    CRUD operations

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,11 +2,25 @@
 
 class User_model extends CI_Model{
 
-    //these methods  communicate directly to our data base
+    //these methods  communicate directly to our database
 
-    public function get_users(){
+    public function get_users($user_id){
 
-        //first we need to communicate with our database. we either auto load it or load it manually
+        $this->db->where('id',$user_id);
+
+        $query = $this->db->get('users');
+
+        return $query-> result();
+
+        //another way of getting the data is
+
+        $this->db->where([
+            'id' => $userid,
+            'username' => $username
+        ]);
+
+
+        //first we need to communicate with our database. we either auto load it or load it manually 
 
         //*****MANUAL CONFIG**********/
         // $config['hostname'] = 'localhost';
@@ -18,9 +32,10 @@ class User_model extends CI_Model{
 
         //We setup an Auto Config from the 
 
-       $query = $this->db->get('users');
+        // $query = $this->db->query("SELECT * FROM users");
 
-        return $query-> result();
+        // return $query-> num_fields(); // this will give out the column count
+        // return $query-> num_row(); // this will give out the row count
 
 
 
@@ -29,7 +44,39 @@ class User_model extends CI_Model{
 
     }
 
+    //CREATE
 
+
+    //after getting variables from the controller, this method creates a user in the database
+
+    public function create_user($data){
+
+
+        $this->db->insert('users',$data);
+
+
+    }
+    
+    //UPDATE
+
+    //after getting variables from the controller, this method updates the database
+
+    public function update_user($data, $id){
+
+        $this->db->where(['user_id' => $id]);
+        $this->db->insert('users',$data);
+
+
+    }
+
+    //DELETE
+
+    public function delete_user($id){
+
+        $this->db->where(['id' => $id]);
+        $this->db->delete('users');
+
+    }
 
 
 }
